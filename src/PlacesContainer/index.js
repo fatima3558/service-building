ef07c6d2375fc8070e485cd7131a36524de2deab
Fatima Gomez <fatima3558@gmail.com>
2019-09-05 15:48:54 -0500

    getting nearbly places on places component loading

--- src/PlacesContainer/index.js
+++ src/PlacesContainer/index.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react';
 
 class PlacesContainer extends Component {
-	constructor() {
-		super()
-		this.state = {
+    constructor() {
+        super()
+        this.state = {
+            search: '',
+            list: []
+        }
+    }
 
-		}
-	}
-	
-	render() {
-		return(
-			<div>
-				PLACES
-			</div>
-		)
-	}
+    componentDidMount() {
+        this.getList()
+        this.showList()
+    }
+
+    getList = async () => {
+        const nearbyPlacesResponse = await fetch('http://localhost:8000/places', {
+        	method: 'get',
+        	credentials: 'include'
+        })
+        if(nearbyPlacesResponse.status !== 200) {
+            throw Error('Could not get nearby places')
+        }
+        const nearbyPlaces = await nearbyPlacesResponse.json()
+
+        this.setState({
+            list: [...nearbyPlaces]
+        })
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.currentTarget.name]: e.currentTarget.value
+        })
+    }
+
+    doSearch = (e) => {
+        e.preventDefault()
+        console.log(this.state.search, "what we're about throw at the api!");
+    }
+    
+    render() {
+        console.log(this.state, "state in placesContainer");
+        return(
+            <div>
+                <PlacesList />
+            </div>
+        )
+    }
 }
 
 export default PlacesContainer
